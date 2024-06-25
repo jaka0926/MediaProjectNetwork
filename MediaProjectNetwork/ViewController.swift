8263f445b5c5099ef34991e8a4e3cf3c770e1525
Jaka <jaka0926@naver.com>
2024-06-25 17:56:20 +0900

    Initial Commit

---
+++ MediaProjectNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProjectNetwork
+//
+//  Created by Jaka on 2024-06-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
